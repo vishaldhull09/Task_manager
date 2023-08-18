b7b17d6c3b15a1444b5c03799fc5c8cab372545d
Vishal <vishaldhull@vishals-MacBook-Air.local>
2023-08-19 01:54:30 +0530

    missing metadata import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import { promises as fs } from "fs"
 import path from "path"
-// import { Metadata } from "next"
+import { Metadata } from "next"
 import Image from "next/image"
 import { z } from "zod"
 import { useTheme } from "next-themes"
